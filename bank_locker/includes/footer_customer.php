c7af0f883d1f6ef12450cb830fbe528075156564
RintoMReji <gamerwsasi@gmail.com>
2026-05-22 19:42:32 +0530

    Delete bank_locker directory

--- bank_locker/includes/footer_customer.php
+++
@@ -1,9 +0,0 @@
-</div></div></div>
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.alert').forEach(function(el) {
-    setTimeout(function(){ el.style.display='none'; }, 4000);
-  });
-});
-</script>
-</body></html>
